c22a79b10bae16e0f8c563a6371c1b0243740452
giankarl30 <158039526+giankarl30@users.noreply.github.com>
2024-01-29 18:52:23 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["shop here!","buy now!"];
 
 
 let currentIndex = 0;
